9099085e1172982b31701b6b86064cff6f388f6b
Yash Sen <111111808+YashSen27@users.noreply.github.com>
2023-01-30 09:51:20 +0530

    Add files via upload

---
+++ files2/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <img src="https://weftse.csb.app/logo.png" alt="logo" className="a" />
+      <img src="https://weftse.csb.app/text.png" alt="logo" className="b" />
+      <img src="https://weftse.csb.app/search.png" alt="logo" className="c" />
+    </div>
+  );
+}
